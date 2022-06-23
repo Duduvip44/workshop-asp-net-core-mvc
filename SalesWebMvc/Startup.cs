be6ca3be0bc2bb51e6f2e9e35a608b87724535af
Eduardo Sousa <essousa7@gmail.com>
2022-06-23 09:21:40 -0300

    Implementing grouping search new

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -41,7 +41,7 @@ namespace SalesWebMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMvcContext>(options =>
-                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                     options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
                         builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
